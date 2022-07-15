67406f5386a616801b25439b2cb57ee10ea07a66
sid2x <siddhartha.eidiko@gmail.com>
2022-07-15 15:09:14 +0530

    added test case

--- src/test/java/com/oa/OaProjectApplicationTests.java
+++ src/test/java/com/oa/OaProjectApplicationTests.java
@@ -1,5 +1,7 @@
 package com.oa;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,8 @@ class OaProjectApplicationTests {
 
 	@Test
 	void contextLoads() {
+		String str="";
+		assertEquals("", str);
 	}
 
 }
